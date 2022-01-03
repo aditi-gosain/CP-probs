b3b90bf24fe74f5d1620e4e4644f94899a65e2c4
Aditi Singh <78258976+aditi-gosain@users.noreply.github.com>
2022-01-03 18:39:54 +0530

    Create at-frog-1-DP.cpp

---
+++ at-frog-1-DP.cpp
@@ -0,0 +1,40 @@
+/*Iterative solution*/
+#include <bits/stdc++.h>
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define int long long int
+#define ld long double
+#define rem 1000000007
+#define fi first
+#define se second
+#define MAX 1000000000
+#define pb push_back
+#define mp make_pair
+using namespace std;
+const ld eps = 1e-6;
+
+vector<int> h;
+vector<int> dp;
+
+int32_t main()
+{
+    int n;
+    cin >> n;
+    h.resize(n);
+    dp.resize(n);
+    for (int i = 0; i < n; i++)
+        cin >> h[i];
+    for (int i = 0; i < n; i++)
+        dp[i] = -1;
+    dp[0] = 0;
+    dp[1] = abs(h[1] - h[0]);
+    for (int i = 2; i < n; i++)
+    {
+        int lastcost = dp[i - 1] + abs(h[i] - h[i - 1]);
+        int lastlastcost = dp[i - 2] + abs(h[i] - h[i - 2]);
+        dp[i] = min(lastcost, lastlastcost);
+    }
+    cout << dp[n - 1];
+}
